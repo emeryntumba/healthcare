f289d54f0da3367ef8e44eb671fdf4b8d03764f5
Emery <emeryntumba39@outlook.com>
2025-04-21 15:33:16 +0100

    Add AuthentificationController and update routes for login and registration

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthentificationController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/login', [AuthentificationController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthentificationController::class, 'login'])->name('login.post');
+Route::get('/logout', [AuthentificationController::class, 'logout'])->name('logout');
+Route::get('/register', [AuthentificationController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [AuthentificationController::class, 'register'])->name('register.post');
